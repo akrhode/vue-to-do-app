b0e204724729321a5dbb912cf9d950116e0673b0
akrhode <a.rhode@web.de>
2023-04-02 23:23:17 +0200

    Build and style app container

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,13 @@ import Todo from "./components/Todo.vue";
 </script>
 
 <template>
-  <div>
+  <div class="bg-gray-600 text-white w-1/2 m-auto mt-3">
+    <div class="text-center border-b-2 border-black py-4">
+      <h1 class="text-3xl py-4">To Do App</h1>
+      <p class="text-xl">To complete:</p>
+    </div>
+    <Todo />
+    <Todo />
     <Todo />
   </div>
 </template>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,7 +1,10 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h1>To Do App</h1>
+  <div
+    class="flex justify-between py-2 px-2 border-2 border-black items-center"
+  >
+    <p class="cursor-pointer">To Do</p>
+    <button class="bg-gray-400 px-2 py-2 rounded">Delete</button>
   </div>
 </template>
