c4a016b3bac76daa680ca8b6aace99b92937c3f1
akrhode <a.rhode@web.de>
2023-04-03 20:53:41 +0200

    Add app functions and to do count

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,85 @@
-<script setup>
-import Todo from "./components/Todo.vue";
-</script>
-
 <template>
-  <div class="bg-gray-600 text-white w-1/2 m-auto mt-3">
-    <div class="text-center border-2 border-black py-4">
+  <div
+    class="text-white bg-gray-600 w-2/4 max-w-2xl m-auto mt-12 border-2 border-black"
+  >
+    <div class="text-center border-b-2 border-black py-4">
       <h1 class="text-3xl py-4">To Do App</h1>
-      <p class="text-xl">To complete:</p>
-      <div class="flex justify-between py-2">
-        <input type="text" class="py-2 w-5/6" />
-        <button class="py-2 w-1/6 bg-blue-500">Add</button>
+
+      <p class="text-xl p-2" v-if="openTodos.length > 0">
+        To complete: {{ openTodos.length }}
+      </p>
+      <p class="text-xl p-2" v-else>Nothing to do!</p>
+      <div class="flex space-around">
+        <input
+          type="text"
+          v-model="newTodo"
+          class="py-2 px-2 text-black inline-block w-5/6"
+        />
+        <button class="w-1/6 bg-blue-500" @click="addTodo">Add Todo</button>
       </div>
     </div>
-    <Todo />
-    <Todo />
-    <Todo />
+    <div v-for="(todo, index) in todos" :key="todo.todo">
+      <Todo
+        :todoprop="todo"
+        :todoindex="index"
+        @remove-index="removeTodo"
+        @toggledone-index="setDone"
+      />
+    </div>
   </div>
 </template>
+
+<script>
+import Todo from "./components/Todo.vue";
+export default {
+  name: "App",
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  mounted() {
+    let data = localStorage.getItem("todos");
+    console.log(data);
+    if (data !== "" && data !== null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
+  methods: {
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.todos));
+    },
+    addTodo() {
+      if (this.newTodo.trim() === "") {
+        return;
+      }
+      this.todos.push({ todo: this.newTodo, done: false });
+      this.storeTodos();
+      this.newTodo = "";
+    },
+    setDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+      this.storeTodos();
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+      this.storeTodos();
+    },
+  },
+  computed: {
+    openTodos() {
+      console.log(this.todos);
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
+    },
+  },
+};
+</script>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,10 +1,33 @@
-<script setup></script>
-
 <template>
   <div
-    class="flex justify-between py-2 px-2 border-2 border-black items-center"
+    class="flex justify-between items-center py-4 px-2 border-t-2 border-black"
+    :class="{ 'bg-green-700': this.todo.done, 'bg-red-700': !this.todo.done }"
   >
-    <p class="cursor-pointer">To Do</p>
-    <button class="bg-gray-400 px-2 py-2 rounded">Delete</button>
+    <p class="cursor-pointer" @click="toggleTodo">
+      {{ todo.todo }}
+    </p>
+    <button class="py-2 px-4 bg-gray-400 rounded" @click="removeTodo">
+      Delete
+    </button>
   </div>
 </template>
+
+<script>
+export default {
+  name: "Todo",
+  props: ["todoprop", "todoindex"],
+  data() {
+    return {
+      todo: this.todoprop,
+    };
+  },
+  methods: {
+    toggleTodo() {
+      this.$emit("toggledone-index", this.todoindex);
+    },
+    removeTodo() {
+      this.$emit("remove-index", this.todoindex);
+    },
+  },
+};
+</script>
